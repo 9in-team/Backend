f5d2af20bf2b9a2af7b2530ac7063fba569686db
deepredk <deepred.kim@gmail.com>
2023-02-17 15:53:04 +0900

    refactor: 불필요한 {} 삭제

--- api/src/main/kotlin/team/guin/api/example/controller/response/AddResponse.kt
+++ api/src/main/kotlin/team/guin/api/example/controller/response/AddResponse.kt
@@ -2,5 +2,4 @@ package team.guin.api.example.controller.response
 
 import team.guin.domain.example.ExampleEntity
 
-data class AddResponse(val added: ExampleEntity) {
-}
+data class AddResponse(val added: ExampleEntity)
