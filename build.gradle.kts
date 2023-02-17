f0674129c67627dd02aa04df8ff89fe0d8ca1c10
deepredk <deepred.kim@gmail.com>
2023-02-17 15:48:16 +0900

    chore: JPA 디펜던시 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,18 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.7.22"
     id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
-    id("org.springframework.boot") version "2.7.8" apply false
-    id("io.spring.dependency-management") version "1.1.0" apply false
-    kotlin("plugin.spring") version "1.7.22" apply false
+    kotlin("plugin.spring") version "1.7.22"
+    id("org.springframework.boot") version "2.7.8"
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("plugin.jpa") version "1.4.10" // wrapped no-arg (kotlin("plugin.noarg") version "1.4.10" 을 포함)
+}
+
+allOpen {
+    annotation("javax.persistence.Entity") // @Entity가 붙은 클래스에 한해서만 all open 플러그인을 적용
+}
+
+noArg {
+    annotation("javax.persistence.Entity") // @Entity가 붙은 클래스에 한해서만 no arg 플러그인을 적용
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_11
@@ -64,5 +73,18 @@ project(":api") {
     dependencies {
         implementation(project(":domain"))
         implementation("org.springframework.boot:spring-boot-starter-web")
+        testImplementation("io.kotest:kotest-runner-junit5:5.5.4")
+        testImplementation("io.kotest:kotest-assertions-core:5.5.4")
+        testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
+    }
+}
+
+project(":domain") {
+    dependencies {
+        runtimeOnly("com.mysql:mysql-connector-j")
+        api("org.springframework.boot:spring-boot-starter-data-jpa")
+        implementation("com.h2database:h2:2.1.214")
+        implementation("org.modelmapper:modelmapper:3.1.1")
+        implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     }
 }
