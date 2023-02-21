d3b99d5882f5974a46563afd4e03f5d1c00c8af3
cousim46 <wjdghldns24@naver.com>
2023-02-21 19:48:18 +0900

    ExampleEntity -> Example 엔티티명 변경, 기본키 null 허용하지 않게 변경

--- domain/src/main/kotlin/team/guin/example/Example.kt
+++ domain/src/main/kotlin/team/guin/example/Example.kt
@@ -1,13 +1,15 @@
 package team.guin.example
 
+import team.guin.BaseEntity
+import java.lang.reflect.Constructor
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity
-class ExampleEntity() {
+class Example(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Long? = null
-}
+    val id: Long= 0,
+)
