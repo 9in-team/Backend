1f735844fc4314b2b537aa7a458272ec10c768c0
cousim46 <wjdghldns24@naver.com>
2023-02-22 00:14:30 +0900

    getList -> findList로 메서드명 변경, Service레이어에 있던 dto 컨트롤러로 변경

--- api/src/main/kotlin/team/guin/example/controller/ExampleController.kt
+++ api/src/main/kotlin/team/guin/example/controller/ExampleController.kt
@@ -5,15 +5,17 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
+import team.guin.example.controller.response.AddResponse
+import team.guin.example.controller.response.CommonListResponse
 import team.guin.example.service.ExampleService
 
 @RestController
 class ExampleController(val exampleService: ExampleService) {
     @PostMapping("/example")
-    fun create() = exampleService.create()
+    fun create() = AddResponse(exampleService.create())
 
     @GetMapping("/example")
-    fun showList() = exampleService.getList()
+    fun showList() = CommonListResponse(exampleService.findList())
 
     @GetMapping("/example/{id}")
     fun showOne(@PathVariable id: Long) = exampleService.get(id)
