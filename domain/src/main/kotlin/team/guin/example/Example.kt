031b6001228389ed81115dc13bbe7c5288caecf4
cousim46 <wjdghldns24@naver.com>
2023-02-21 20:19:44 +0900

    BaseEntity 상속

--- domain/src/main/kotlin/team/guin/example/Example.kt
+++ domain/src/main/kotlin/team/guin/example/Example.kt
@@ -1,15 +1,7 @@
 package team.guin.example
 
 import team.guin.BaseEntity
-import java.lang.reflect.Constructor
 import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
 
 @Entity
-class Example(
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long= 0,
-)
+class Example : BaseEntity()
