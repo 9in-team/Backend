93b29e0bfe519aab77f422a4207b09cbc0b83fe4
cousim46 <wjdghldns24@naver.com>
2023-02-21 19:50:23 +0900

    Auditing을 위한 설정

--- api/src/main/kotlin/team/guin/ApiApplication.kt
+++ api/src/main/kotlin/team/guin/ApiApplication.kt
@@ -2,8 +2,10 @@ package team.guin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class ApiApplication
 
 fun main(args: Array<String>) {
