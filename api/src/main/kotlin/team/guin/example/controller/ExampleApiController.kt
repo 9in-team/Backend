2fe0ec1d556d1344e582ee9aac7b5723ffc776f2
cousim46 <wjdghldns24@naver.com>
2023-02-22 20:21:54 +0900

    RequestMapping을 이용하여 공통 url 적용

--- api/src/main/kotlin/team/guin/example/controller/ExampleApiController.kt
+++ api/src/main/kotlin/team/guin/example/controller/ExampleApiController.kt
@@ -4,22 +4,24 @@ import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import team.guin.example.controller.response.AddResponse
 import team.guin.example.controller.response.CommonListResponse
 import team.guin.example.service.ExampleService
 
+@RequestMapping("/example")
 @RestController
-class ExampleController(val exampleService: ExampleService) {
-    @PostMapping("/example")
+class ExampleApiController(val exampleService: ExampleService) {
+    @PostMapping
     fun create() = AddResponse(exampleService.create())
 
-    @GetMapping("/example")
+    @GetMapping
     fun showList() = CommonListResponse(exampleService.findList())
 
-    @GetMapping("/example/{id}")
+    @GetMapping("/{id}")
     fun showOne(@PathVariable id: Long) = exampleService.get(id)
 
-    @DeleteMapping("/example/{id}")
+    @DeleteMapping("/{id}")
     fun delete(@PathVariable id: Long) = exampleService.delete(id)
 }
