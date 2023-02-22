42c7de1742e445885661f357028afd63917911b5
cousim46 <wjdghldns24@naver.com>
2023-02-22 22:35:07 +0900

    엔티티 컬럼 추가

--- domain/src/main/kotlin/team/guin/example/Example.kt
+++ domain/src/main/kotlin/team/guin/example/Example.kt
@@ -1,7 +1,13 @@
 package team.guin.example
 
 import team.guin.BaseEntity
+import javax.persistence.Column
 import javax.persistence.Entity
 
 @Entity
-class Example : BaseEntity()
+class Example(
+    @Column
+    val name: String,
+    @Column
+    val age: Int,
+) : BaseEntity()
