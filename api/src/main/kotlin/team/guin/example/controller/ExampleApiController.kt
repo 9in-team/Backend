a3c33c159c90a602dc7ac4152f1f4c4aa3de6450
cousim46 <wjdghldns24@naver.com>
2023-02-22 22:36:02 +0900

    요청받는 Dto 추가

--- api/src/main/kotlin/team/guin/example/controller/ExampleApiController.kt
+++ api/src/main/kotlin/team/guin/example/controller/ExampleApiController.kt
@@ -4,17 +4,19 @@ import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import team.guin.example.Example
+import team.guin.example.controller.request.ExampleCreateRequest
 import team.guin.example.service.ExampleService
 
 @RequestMapping("/example")
 @RestController
 class ExampleApiController(val exampleService: ExampleService) {
     @PostMapping
-    fun createExample(): Example =
-        exampleService.createExample()
+    fun createExample(@RequestBody request: ExampleCreateRequest): Example =
+        exampleService.createExample(request.name, request.age)
 
     @GetMapping
     fun findAllExample(): List<Example> =
