04e8db26132d4ef2b164cf8bb09f1d761a79a08d
cousim46 <wjdghldns24@naver.com>
2023-02-23 23:55:09 +0900

    JpaRepository -> 공통 Repository로 변경

--- api/src/main/kotlin/team/guin/example/repository/ExampleRepository.kt
+++ api/src/main/kotlin/team/guin/example/repository/ExampleRepository.kt
@@ -1,8 +1,8 @@
 package team.guin.example.repository
 
-import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import team.guin.BaseRepository
 import team.guin.example.Example
 
 @Repository
-interface ExampleRepository : JpaRepository<Example, Long>
+interface ExampleRepository : BaseRepository<Example, Long>
