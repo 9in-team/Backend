3b0c26c74cdf0c7794a4685d894299c3cb7e9b12
cousim46 <wjdghldns24@naver.com>
2023-02-23 23:54:18 +0900

    불필요한 코드 삭제

--- domain/src/main/kotlin/team/guin/example/Example.kt
+++ domain/src/main/kotlin/team/guin/example/Example.kt
@@ -1,12 +1,10 @@
 package team.guin.example
 
-import org.hibernate.annotations.SQLDelete
 import org.hibernate.annotations.Where
 import team.guin.BaseEntity
 import javax.persistence.Column
 import javax.persistence.Entity
 
-@SQLDelete(sql = "UPDATE example SET deleted_at = now() where id = ?")
 @Where(clause = "deleted_at IS NULL")
 @Entity
 class Example(
