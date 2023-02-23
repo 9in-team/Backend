66c68e38b3ab9a7961aa4b10d39b6ba099e6a48d
cousim46 <wjdghldns24@naver.com>
2023-02-23 23:54:00 +0900

    공통 Repository

---
+++ domain/src/main/kotlin/team/guin/BaseRepository.kt
@@ -0,0 +1,21 @@
+package team.guin
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.NoRepositoryBean
+import org.springframework.data.repository.query.Param
+import java.io.Serializable
+
+@NoRepositoryBean
+interface BaseRepository<T : BaseEntity, ID : Serializable> : JpaRepository<T, ID> {
+    @Query("UPDATE #{#entityName} e SET e.deletedAt = CURRENT_TIMESTAMP WHERE e.id = :id")
+    @Modifying
+    override fun deleteById(@Param("id") id: ID)
+
+    @Query("UPDATE #{#entityName} e SET e.deletedAt = CURRENT_TIMESTAMP WHERE e IN :entities")
+    @Modifying
+    override fun deleteAll(@Param("entities") entities: MutableIterable<T>)
+
+    override fun deleteAllById(ids: MutableIterable<ID>)
+}
