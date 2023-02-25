737478e17c2e698835adf60a62bb78d4facf3b03
deepredk <deepred.kim@gmail.com>
2023-02-25 12:53:15 +0900

    refactor: AccountApiController 클래스 자체에 @RequestMapping("/account") 어노테이션 부여

--- api/src/main/kotlin/team/guin/api/controller/AccountApiController.kt
+++ api/src/main/kotlin/team/guin/api/controller/AccountApiController.kt
@@ -2,6 +2,7 @@ package team.guin.api.controller
 
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import team.guin.api.controller.request.AccountJoinRequest
 import team.guin.api.controller.response.CommonResponse
@@ -9,10 +10,11 @@ import team.guin.api.controller.response.detail.AccountDetail
 import team.guin.api.service.AccountApiService
 
 @RestController
+@RequestMapping("/account")
 class AccountApiController(
     private val accountApiService: AccountApiService,
 ) {
-    @PostMapping("/account")
+    @PostMapping
     fun join(@RequestBody accountJoinRequest: AccountJoinRequest): CommonResponse<AccountDetail> {
         val account = accountApiService.join(accountJoinRequest.email, accountJoinRequest.nickname, accountJoinRequest.imageId)
         val detail = AccountDetail(account.email, account.nickname, account.imageId)
