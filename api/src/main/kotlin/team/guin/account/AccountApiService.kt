14c6e85580bf356444e23fe1133e2cb2c4c34eaf
cousim46 <wjdghldns24@naver.com>
2023-02-28 19:51:43 +0900

    회원정보 수정에 대한 비즈니스 로직

--- api/src/main/kotlin/team/guin/account/AccountApiService.kt
+++ api/src/main/kotlin/team/guin/account/AccountApiService.kt
@@ -1,5 +1,6 @@
 package team.guin.account
 
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import team.guin.domain.account.Account
 
@@ -10,5 +11,15 @@ class AccountApiService(
     fun join(email: String, nickname: String, imageId: String): Account {
         val account = Account.create(email, nickname, imageId)
         return accountApiRepository.save(account)
+    }
+
+    fun updateInfo(id: Long, email: String, nickname: String, imageId: String): Account {
+        val account = this.findByAccountId(id)
+        account.updateInfo(email, nickname, imageId)
+        return account
+    }
+
+    private fun findByAccountId(id: Long): Account {
+        return accountApiRepository.findByIdOrNull(id) ?: throw IllegalArgumentException("유저가 존재하지 않습니다.")
     }
 }
