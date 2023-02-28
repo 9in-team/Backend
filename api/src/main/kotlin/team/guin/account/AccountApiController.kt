e7aeda885363c5e87f506c99a842e75be53e1df8
cousim46 <wjdghldns24@naver.com>
2023-02-28 19:50:41 +0900

    회원정보 수정에 대한 API

--- api/src/main/kotlin/team/guin/account/AccountApiController.kt
+++ api/src/main/kotlin/team/guin/account/AccountApiController.kt
@@ -1,11 +1,14 @@
 package team.guin.account
 
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import team.guin.account.dto.AccountDetail
 import team.guin.account.dto.AccountJoinRequest
+import team.guin.account.dto.AccountUpdateRequest
 import team.guin.common.CommonResponse
 
 @RestController
@@ -18,5 +21,11 @@ class AccountApiController(
         val account = accountApiService.join(accountJoinRequest.email, accountJoinRequest.nickname, accountJoinRequest.imageId)
         val detail = AccountDetail(account.email, account.nickname, account.imageId)
         return CommonResponse.okWithDetail(detail)
+    }
+
+    @PutMapping("{id}")
+    fun update(@PathVariable id: Long, @RequestBody accountDetail: AccountUpdateRequest): CommonResponse<AccountDetail> {
+        val account = accountApiService.updateInfo(id, accountDetail.email, accountDetail.nickname, accountDetail.imageId)
+        return CommonResponse.okWithDetail(AccountDetail(account.email, account.nickname, account.imageId))
     }
 }
