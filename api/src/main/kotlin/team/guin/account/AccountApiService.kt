9ce07342754bad87a48222a83913737db346ab8c
cousim46 <wjdghldns24@naver.com>
2023-02-28 20:45:14 +0900

    @Transactional 추가

--- api/src/main/kotlin/team/guin/account/AccountApiService.kt
+++ api/src/main/kotlin/team/guin/account/AccountApiService.kt
@@ -2,17 +2,21 @@ package team.guin.account
 
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import team.guin.domain.account.Account
 
 @Service
+@Transactional(readOnly = true)
 class AccountApiService(
     private val accountApiRepository: AccountApiRepository,
 ) {
+    @Transactional
     fun join(email: String, nickname: String, imageId: String): Account {
         val account = Account.create(email, nickname, imageId)
         return accountApiRepository.save(account)
     }
 
+    @Transactional
     fun updateInfo(id: Long, email: String, nickname: String, imageId: String): Account {
         val account = this.findByAccountId(id)
         account.updateInfo(email, nickname, imageId)
