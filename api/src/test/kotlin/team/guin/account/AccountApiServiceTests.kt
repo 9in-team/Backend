f8badc9a476ee91e60dc2be85d8f36151df5c883
cousim46 <wjdghldns24@naver.com>
2023-02-28 19:52:06 +0900

    회원정보 수정에 대한 비즈니스 로직 테스트코드

--- api/src/test/kotlin/team/guin/account/AccountApiServiceTests.kt
+++ api/src/test/kotlin/team/guin/account/AccountApiServiceTests.kt
@@ -1,10 +1,13 @@
 package team.guin.account
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.springframework.boot.test.context.SpringBootTest
+import team.guin.domain.account.Account
+import team.guin.domain.account.enumeration.AccountRoles
 
 @SpringBootTest
 class AccountApiServiceTests(
@@ -23,7 +26,7 @@ class AccountApiServiceTests(
 
             // then
             withContext(Dispatchers.IO) {
-                val optAccountEntity = accountApiRepository.findById(account.userId)
+                val optAccountEntity = accountApiRepository.findById(account.id)
                 optAccountEntity.isEmpty shouldBe false
 
                 val accountEntity = optAccountEntity.get()
@@ -31,6 +34,47 @@ class AccountApiServiceTests(
                 accountEntity.nickname shouldBe nickname
                 accountEntity.imageId shouldBe imageId
             }
+        }
+    }
+    "updateInfo" - {
+        "회원이 정보를 변경하면 정보를 업데이트 한다." - {
+            // given
+            val email = "a@a.com"
+            val nickname = "nickname"
+            val imageId = "https://imugr.com/example"
+            val savedAccount = accountApiRepository.save(Account(email, nickname, imageId, AccountRoles.USER))
+
+            // when
+            val changeEmail = "test@a.com"
+            val changeNickname = "changeNickname"
+            val changeImageId = "https://imugr.com/change"
+            val updatedAccount =
+                accountApiService.updateInfo(savedAccount.id, changeEmail, changeNickname, changeImageId)
+
+            // then
+            updatedAccount.email shouldBe changeEmail
+            updatedAccount.imageId shouldBe changeImageId
+            updatedAccount.nickname shouldBe changeNickname
+        }
+
+        "해당하는 AccountId 존재하지 않을경우 예외가 발생한다." - {
+            // given
+            val email = "a@a.com"
+            val nickname = "nickname"
+            val imageId = "https://imugr.com/example"
+            // && when
+            val exception =
+                shouldThrow<IllegalArgumentException> {
+                    accountApiService.updateInfo(
+                        -1,
+                        email,
+                        nickname,
+                        imageId,
+                    )
+                }
+
+            // then
+            exception.message shouldBe "유저가 존재하지 않습니다."
         }
     }
 })
