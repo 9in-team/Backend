ce2451ceec56c18e5e84b36cd28f27acfcd2f3ad
cousim46 <wjdghldns24@naver.com>
2023-03-01 18:31:56 +0900

    - 불필요항 Transactional 삭제 - findByAccountId -> findById로 수정

--- api/src/main/kotlin/team/guin/account/AccountApiService.kt
+++ api/src/main/kotlin/team/guin/account/AccountApiService.kt
@@ -6,11 +6,9 @@ import org.springframework.transaction.annotation.Transactional
 import team.guin.domain.account.Account
 
 @Service
-@Transactional(readOnly = true)
 class AccountApiService(
     private val accountApiRepository: AccountApiRepository,
 ) {
-    @Transactional
     fun join(email: String, nickname: String, imageId: String): Account {
         val account = Account.create(email, nickname, imageId)
         return accountApiRepository.save(account)
@@ -18,12 +16,12 @@ class AccountApiService(
 
     @Transactional
     fun updateInfo(id: Long, nickname: String, imageId: String): Account {
-        val account = this.findByAccountId(id)
+        val account = this.findById(id)
         account.updateInfo(nickname, imageId)
         return account
     }
 
-    private fun findByAccountId(id: Long): Account {
+    private fun findById(id: Long): Account {
         return accountApiRepository.findByIdOrNull(id) ?: throw IllegalArgumentException("유저가 존재하지 않습니다.")
     }
 }
