c26e8dba4ca53e3a39da25e198d018d79095b028
deepredk <deepred.kim@gmail.com>
2023-03-04 20:43:13 +0900

    feat: POST /login 구현

---
+++ api/src/main/kotlin/team/guin/security/kakao/KakaoAuthenticationManager.kt
@@ -0,0 +1,25 @@
+package team.guin.security.kakao
+
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.stereotype.Component
+import team.guin.account.AccountApiRepository
+import team.guin.domain.account.Account
+
+@Component
+class KakaoAuthenticationManager(
+    private val accountApiRepository: AccountApiRepository
+): AuthenticationManager {
+    override fun authenticate(authentication: Authentication): Authentication {
+        val kakaoUserInfo = authentication.details as KakaoUserInfo
+        print(kakaoUserInfo.nickname)
+        val account = accountApiRepository.findByEmail(authentication.name)
+            ?: accountApiRepository.save(Account.create(kakaoUserInfo.email, kakaoUserInfo.nickname, kakaoUserInfo.profileImageUrl))
+
+        (authentication.authorities as MutableList<GrantedAuthority>).add(SimpleGrantedAuthority(account.accountRole.toString()))
+        return authentication
+    }
+}
