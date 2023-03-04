672477a4be981e440541765577d57710cc0f7337
deepredk <deepred.kim@gmail.com>
2023-03-04 21:00:39 +0900

    refactor: ktlint 준수

--- api/src/main/kotlin/team/guin/security/SecurityConfig.kt
+++ api/src/main/kotlin/team/guin/security/SecurityConfig.kt
@@ -4,15 +4,13 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import team.guin.security.kakao.KakaoAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig(
-    private val kakaoAuthenticationFilter: KakaoAuthenticationFilter
+    private val kakaoAuthenticationFilter: KakaoAuthenticationFilter,
 ) {
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
