e51e94cb1a0f4076b0c09364989835790eadb815
choikangheon <choikangheon@naver.com>
2023-03-05 12:41:23 +0900

    [#55] feat web-common 멀티 모듈 프로젝트 구성 아래 도메인 같이 공통적인 기능들 적용예정   - Global Exception   - AOP

---
+++ web-common/src/main/kotlin/team/guin/webcommon/WebCommonApplication.kt
@@ -0,0 +1,11 @@
+package team.guin.webcommon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebCommonApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebCommonApplication>(*args)
+}
