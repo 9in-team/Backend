f1fabc12a2123210262a52137d3f06a397029191
cousim46 <wjdghldns24@naver.com>
2023-03-11 23:04:49 +0900

    TeamRole Entity 생성

---
+++ domain/src/main/kotlin/team/guin/domain/team/TeamRole.kt
@@ -0,0 +1,19 @@
+package team.guin.domain.team
+
+import team.guin.domain.baseentity.BaseEntity
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.FetchType
+import javax.persistence.ManyToOne
+
+@Entity
+class TeamRole(
+    @ManyToOne(fetch = FetchType.LAZY)
+    var team: Team,
+    @Column(nullable = false)
+    var roleName: String,
+    @Column(nullable = false)
+    var roleRequired: Int,
+    @Column(nullable = false)
+    var roleHired: Int,
+) : BaseEntity()
