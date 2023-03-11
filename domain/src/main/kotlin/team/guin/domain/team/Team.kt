4133d84cc0f147172f09476dc38041c776239603
cousim46 <wjdghldns24@naver.com>
2023-03-11 23:00:23 +0900

    팀에 대한 Entityˆ

---
+++ domain/src/main/kotlin/team/guin/domain/team/Team.kt
@@ -0,0 +1,20 @@
+package team.guin.domain.team
+
+import team.guin.domain.account.Account
+import team.guin.domain.baseentity.BaseEntity
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.FetchType
+import javax.persistence.ManyToOne
+
+@Entity
+class Team(
+    @ManyToOne(fetch = FetchType.LAZY)
+    var account: Account,
+    @Column(nullable = false)
+    var teamSubject: String,
+    @Column(nullable = false)
+    var teamContent: String,
+    @Column(nullable = false)
+    var teamOpenChatUrl: String,
+) : BaseEntity()
