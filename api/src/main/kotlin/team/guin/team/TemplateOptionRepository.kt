c174655bbbb81779cb67a2feefe5a3e45fd57ecd
cousim46 <wjdghldns24@naver.com>
2023-03-12 21:54:07 +0900

    TemplateOptionRepository repository 생성

---
+++ api/src/main/kotlin/team/guin/team/TemplateOptionRepository.kt
@@ -0,0 +1,8 @@
+package team.guin.team
+
+import org.springframework.stereotype.Repository
+import team.guin.domain.baseentity.BaseRepository
+import team.guin.domain.team.TemplateOption
+
+@Repository
+interface TemplateOptionRepository : BaseRepository<TemplateOption, Long>
