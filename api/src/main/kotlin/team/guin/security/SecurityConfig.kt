22cff4fb7d50183b50242242449e4ba67f464ec9
deepredk <deepred.kim@gmail.com>
2023-03-13 19:50:37 +0900

    지금까지 작업한 부분 커밋

--- api/src/main/kotlin/team/guin/security/SecurityConfig.kt
+++ api/src/main/kotlin/team/guin/security/SecurityConfig.kt
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import team.guin.domain.account.enumeration.AccountRole
 import team.guin.security.kakao.KakaoAuthenticationFilter
@@ -16,10 +17,16 @@ class SecurityConfig(
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         return http
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+            .sessionFixation().migrateSession()
+            .maximumSessions(1).maxSessionsPreventsLogin(false)
+            .and().and()
             .cors().and()
             .csrf().disable()
             .authorizeHttpRequests()
             .antMatchers("/login").permitAll()
+            .antMatchers("/example/**").permitAll()
             .antMatchers("/api/**").hasRole(AccountRole.USER.toString())
             .antMatchers("/api/admin").hasRole(AccountRole.ADMIN.toString())
             .anyRequest().authenticated()
