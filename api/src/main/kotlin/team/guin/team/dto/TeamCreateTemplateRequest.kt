03437e24d1bf2ab0aeadf044f4abb4331ab64f0b
cousim46 <wjdghldns24@naver.com>
2023-03-19 21:12:29 +0900

    TeamCreateTemplateRequest 생성

--- api/src/main/kotlin/team/guin/team/dto/TeamCreateTemplateRequest.kt
+++ api/src/main/kotlin/team/guin/team/dto/TeamCreateTemplateRequest.kt
@@ -1,9 +1,18 @@
 package team.guin.team.dto
 
+import team.guin.domain.team.dto.TeamTemplateCreate
 import team.guin.domain.team.enumeration.TemplateType
 
 data class TeamCreateTemplateRequest(
     val type: TemplateType,
     val question: String,
-    val options: MutableList<String>? = null,
-)
+    val options: List<String>? = null,
+) {
+    fun toDomain(): TeamTemplateCreate {
+        return TeamTemplateCreate(
+            templateType = type,
+            templateQuestion = question,
+            options = options,
+        )
+    }
+}
