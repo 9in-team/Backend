87e7d08a3ede5c19304240c5764f39e6f39c54a4
cousim46 <wjdghldns24@naver.com>
2023-03-19 21:13:36 +0900

    TeamCreateRoleRequest 생성

--- api/src/main/kotlin/team/guin/team/dto/TeamCreateRoleRequest.kt
+++ api/src/main/kotlin/team/guin/team/dto/TeamCreateRoleRequest.kt
@@ -1,6 +1,15 @@
 package team.guin.team.dto
 
+import team.guin.domain.team.dto.TeamRoleCreate
+
 data class TeamCreateRoleRequest(
     val roleName: String,
     val roleRequired: Int,
-)
+) {
+    fun toDomain(): TeamRoleCreate {
+        return TeamRoleCreate(
+            roleName = roleName,
+            roleRequired = roleRequired,
+        )
+    }
+}
