2592277e8bacc338e175de58a51e9c7099934e9d
cousim46 <wjdghldns24@naver.com>
2023-03-27 21:56:08 +0900

    TeamApiController 생성

---
+++ api/src/main/kotlin/team/guin/team/TeamApiController.kt
@@ -0,0 +1,22 @@
+package team.guin.team
+
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import team.guin.common.CommonResponse
+import team.guin.team.dto.TeamCreateDetail
+import team.guin.team.dto.TeamCreateRequest
+
+@RestController
+@RequestMapping("/team")
+class TeamApiController(
+    private val teamApiService: TeamApiService,
+) {
+    @PostMapping("/{account-id}")
+    fun create(@PathVariable("account-id") accountId: Long, @RequestBody teamCreateRequest: TeamCreateRequest): CommonResponse<TeamCreateDetail> {
+        val savedTeam = teamApiService.create(accountId, teamCreateRequest.toDomain())
+        return CommonResponse.okWithDetail(TeamCreateDetail(savedTeam.id))
+    }
+}
