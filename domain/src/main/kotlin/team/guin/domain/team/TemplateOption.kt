c8d75a7b06ba7ccb03187a3067563ddeed81d995
cousim46 <wjdghldns24@naver.com>
2023-03-27 20:47:25 +0900

    TemplateOption create 팩토리 메서드 생성

--- domain/src/main/kotlin/team/guin/domain/team/TemplateOption.kt
+++ domain/src/main/kotlin/team/guin/domain/team/TemplateOption.kt
@@ -3,13 +3,17 @@ package team.guin.domain.team
 import team.guin.domain.baseentity.BaseEntity
 import javax.persistence.Column
 import javax.persistence.Entity
-import javax.persistence.FetchType
-import javax.persistence.ManyToOne
 
 @Entity
 class TemplateOption(
-    @ManyToOne(fetch = FetchType.LAZY)
-    var teamTemplate: TeamTemplate,
     @Column(nullable = false, length = 150)
     var name: String, // ENUM
-) : BaseEntity()
+) : BaseEntity() {
+    companion object {
+        fun create(
+            name: String,
+        ): TemplateOption {
+            return TemplateOption(name)
+        }
+    }
+}
