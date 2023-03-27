14d9165d95ddf40866efd83dedeca7a7ed923548
cousim46 <wjdghldns24@naver.com>
2023-03-27 21:55:38 +0900

    엔티티 수정

--- domain/src/main/kotlin/team/guin/domain/team/HashTag.kt
+++ domain/src/main/kotlin/team/guin/domain/team/HashTag.kt
@@ -1,23 +1,24 @@
 package team.guin.domain.team
 
 import team.guin.domain.baseentity.BaseEntity
-import team.guin.domain.team.enumeration.HashTagType
+import team.guin.domain.team.enumeration.SubjectType
+import team.guin.domain.team.enumeration.TagType
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.EnumType
 import javax.persistence.Enumerated
+import javax.persistence.JoinColumn
+import javax.persistence.OneToMany
 
 @Entity
 class HashTag(
-    @Column(nullable = false, length = 20)
-    var name: String,
     @Column(nullable = false, length = 10)
     @Enumerated(EnumType.STRING)
-    var type: HashTagType,
-) : BaseEntity() {
-    companion object {
-        fun create(hashTagName: String, hashTagType: HashTagType): HashTag {
-            return HashTag(hashTagName, hashTagType)
-        }
-    }
-}
+    var subjectType: SubjectType,
+    @Column(nullable = false, length = 10)
+    @Enumerated(EnumType.STRING)
+    var type: TagType,
+    @OneToMany
+    @JoinColumn(name = "hash_tag_id")
+    var teamHashTag: MutableList<TeamHashTag>? = null,
+) : BaseEntity()
