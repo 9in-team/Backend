b0fef70e29cfc949a3458b39e337fce55c736777
cousim46 <wjdghldns24@naver.com>
2023-03-27 20:48:19 +0900

    TeamRole create 팩토리 메서드 생성

--- domain/src/main/kotlin/team/guin/domain/team/TeamRole.kt
+++ domain/src/main/kotlin/team/guin/domain/team/TeamRole.kt
@@ -3,17 +3,19 @@ package team.guin.domain.team
 import team.guin.domain.baseentity.BaseEntity
 import javax.persistence.Column
 import javax.persistence.Entity
-import javax.persistence.FetchType
-import javax.persistence.ManyToOne
 
 @Entity
 class TeamRole(
-    @ManyToOne(fetch = FetchType.LAZY)
-    var team: Team? = null,
     @Column(nullable = false, length = 30)
     var name: String,
     @Column(nullable = false)
     var required: Int,
     @Column(nullable = false)
     var hired: Int = 0,
-) : BaseEntity()
+) : BaseEntity() {
+    companion object {
+        fun create(name: String, roleRequired: Int): TeamRole {
+            return TeamRole(name = name, required = roleRequired)
+        }
+    }
+}
