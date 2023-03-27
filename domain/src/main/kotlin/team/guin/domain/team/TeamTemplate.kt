0aaa1be80cfb9176059d1c87a957b9e34773fb55
cousim46 <wjdghldns24@naver.com>
2023-03-27 20:47:59 +0900

    TemplateOption @OneToMany 관계 설정

--- domain/src/main/kotlin/team/guin/domain/team/TeamTemplate.kt
+++ domain/src/main/kotlin/team/guin/domain/team/TeamTemplate.kt
@@ -2,20 +2,22 @@ package team.guin.domain.team
 
 import team.guin.domain.baseentity.BaseEntity
 import team.guin.domain.team.enumeration.TemplateType
+import javax.persistence.CascadeType
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.EnumType
 import javax.persistence.Enumerated
-import javax.persistence.FetchType
-import javax.persistence.ManyToOne
+import javax.persistence.JoinColumn
+import javax.persistence.OneToMany
 
 @Entity
 class TeamTemplate(
-    @ManyToOne(fetch = FetchType.LAZY)
-    var team: Team? = null,
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 30)
     var type: TemplateType,
     @Column(nullable = false, length = 900)
     var question: String,
+    @OneToMany(cascade = [CascadeType.ALL], orphanRemoval = true)
+    @JoinColumn(name = "team_template_id")
+    var options: MutableList<TemplateOption>,
 ) : BaseEntity()
