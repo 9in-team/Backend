871cc33f7badd86267cdb98846b2fc4165307488
cousim46 <wjdghldns24@naver.com>
2023-03-31 23:23:30 +0900

    불필요한 파일 삭제

--- api/src/main/kotlin/team/guin/team/TeamHashTagApiRepository.kt
+++
@@ -1,8 +0,0 @@
-package team.guin.team
-
-import org.springframework.stereotype.Repository
-import team.guin.domain.baseentity.BaseRepository
-import team.guin.domain.team.TeamHashTag
-
-@Repository
-interface TeamHashTagApiRepository : BaseRepository<TeamHashTag, Long>
