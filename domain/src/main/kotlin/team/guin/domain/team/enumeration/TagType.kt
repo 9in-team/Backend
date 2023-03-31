97284beec55c082bafc624052ad2904f2c9e8bcf
cousim46 <wjdghldns24@naver.com>
2023-03-31 23:23:44 +0900

    해시태그에 대한 enum

--- domain/src/main/kotlin/team/guin/domain/team/enumeration/TagType.kt
+++ domain/src/main/kotlin/team/guin/domain/team/enumeration/TagType.kt
@@ -1,8 +1,21 @@
 package team.guin.domain.team.enumeration
 
-enum class TagType(name: String, type: String) {
-    PYTHON("Python", "Common"), SPRING_FRAMEWORK("Spring Framework", "Common"), AWS("AWS", "Common"), IOS("iOS", "Common"),
-    JAVASCRIPT("JavaScript", "Common"), HTML("Html", "Common"), JAVA("Java", "Common"), C_SHARP("C#", "Common"), C_PLUS("C++", "Common"),
-    REACT("React.js", "Common"), NODE("Node.js", "Common"), VUE("Vue.js", "Common"), MYSQL("MySQL", "Common"),
-    KOTLIN("Kotlin", "Common"), ANDROID("Android", "Common"), SQL("SQL", "Common"), ALGORITHM("알고리즘", SubjectType.STUDY.name),
+enum class TagType(name: String, subjectType: List<SubjectType>) {
+    PYTHON("Python", listOf(SubjectType.PROJECT, SubjectType.STUDY)),
+    SPRING_FRAMEWORK("Spring Framework", listOf(SubjectType.PROJECT, SubjectType.STUDY)),
+    AWS("AWS", listOf(SubjectType.PROJECT, SubjectType.STUDY)),
+    IOS("iOS", listOf(SubjectType.PROJECT, SubjectType.STUDY)),
+    JAVASCRIPT("JavaScript", listOf(SubjectType.PROJECT, SubjectType.STUDY)),
+    HTML("Html", listOf(SubjectType.PROJECT, SubjectType.STUDY)),
+    JAVA("Java", listOf(SubjectType.PROJECT, SubjectType.STUDY)),
+    C_SHARP("C#", listOf(SubjectType.PROJECT, SubjectType.STUDY)),
+    C_PLUS("C++", listOf(SubjectType.PROJECT, SubjectType.STUDY)),
+    REACT("React.js", listOf(SubjectType.PROJECT, SubjectType.STUDY)),
+    NODE("Node.js", listOf(SubjectType.PROJECT, SubjectType.STUDY)),
+    VUE("Vue.js", listOf(SubjectType.PROJECT, SubjectType.STUDY)),
+    MYSQL("MySQL", listOf(SubjectType.PROJECT, SubjectType.STUDY)),
+    KOTLIN("Kotlin", listOf(SubjectType.PROJECT, SubjectType.STUDY)),
+    ANDROID("Android", listOf(SubjectType.PROJECT, SubjectType.STUDY)),
+    SQL("SQL", listOf(SubjectType.PROJECT, SubjectType.STUDY)),
+    ALGORITHM("알고리즘", listOf(SubjectType.STUDY)),
 }
