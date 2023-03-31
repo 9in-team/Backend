c16bd7b0e81d970208764c301d01a180f8d4d27c
박민규 <charming@woowahan.com>
2023-03-31 18:42:47 +0900

    [50] 카카오 로그인 디버깅

--- api/src/main/kotlin/team/guin/account/AccountApiController.kt
+++ api/src/main/kotlin/team/guin/account/AccountApiController.kt
@@ -1,5 +1,6 @@
 package team.guin.account
 
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -18,6 +19,9 @@ import javax.servlet.http.HttpSession
 @RequestMapping("/account")
 class AccountApiController(
     private val accountApiService: AccountApiService,
+    private val kakaoApiService: KakaoApiService,
+    private val accountApiRepository: AccountApiRepository,
+    private val springSessionDefaultRedisSerializer: GenericJackson2JsonRedisSerializer,
 ) {
     @PostMapping
     fun join(
---
+++ api/src/main/kotlin/team/guin/security/annotation/AccountLogin.kt
@@ -0,0 +1,5 @@
+package team.guin.security.annotation
+
+@Target(AnnotationTarget.PROPERTY)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class AccountLogin
