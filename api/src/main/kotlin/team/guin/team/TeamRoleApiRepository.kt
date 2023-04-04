693071c318f0f49231e8d201a4818cd4d797f22c
cousim46 <wjdghldns24@naver.com>
2023-04-04 22:45:57 +0900

    불필요한 파일 삭제

--- api/src/main/kotlin/team/guin/team/TeamRoleApiRepository.kt
+++
@@ -1,8 +0,0 @@
-package team.guin.team
-
-import org.springframework.stereotype.Repository
-import team.guin.domain.baseentity.BaseRepository
-import team.guin.domain.team.TeamRole
-
-@Repository
-interface TeamRoleApiRepository : BaseRepository<TeamRole, Long>
--- api/src/main/kotlin/team/guin/team/TeamTemplateApiRepository.kt
+++
@@ -1,8 +0,0 @@
-package team.guin.team
-
-import org.springframework.stereotype.Repository
-import team.guin.domain.baseentity.BaseRepository
-import team.guin.domain.team.TeamTemplate
-
-@Repository
-interface TeamTemplateApiRepository : BaseRepository<TeamTemplate, Long>
