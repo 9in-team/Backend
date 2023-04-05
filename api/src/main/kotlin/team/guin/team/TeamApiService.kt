31d7382127a3e1e94eef0c8a9bad8b0b7afed7dd
cousim46 <wjdghldns24@naver.com>
2023-04-05 21:56:56 +0900

    변수명 변경 및 유저에 대한 처리 수정

--- api/src/main/kotlin/team/guin/team/TeamApiService.kt
+++ api/src/main/kotlin/team/guin/team/TeamApiService.kt
@@ -1,29 +1,30 @@
 package team.guin.team
 
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
-import team.guin.account.AccountApiRepository
+import team.guin.account.AccountApiService
 import team.guin.domain.team.Team
 import team.guin.domain.team.dto.TeamCreate
 
 @Service
 class TeamApiService(
     private val teamApiRepository: TeamApiRepository,
-    private val accountApiRepository: AccountApiRepository,
+    private val accountApiService: AccountApiService,
 ) {
-    @Transactional
     fun createTeam(accountId: Long, teamCreate: TeamCreate): Team {
-        val account = accountApiRepository.findByIdOrNull(accountId) ?: throw IllegalArgumentException("유저 없습니다.")
+        val account = accountApiService.findById(accountId)
         val team = Team.create(
-            account,
+            leader = account,
             subject = teamCreate.subject,
             content = teamCreate.content,
             openChatUrl = teamCreate.openChatUrl,
             templates = teamCreate.templates,
-            roles = teamCreate.roles,
+            parts = teamCreate.parts,
             hashTags = teamCreate.hashTags.toMutableList(),
         )
         return teamApiRepository.save(team)
+    }
+
+    fun findById(id: Long): Team {
+        return teamApiRepository.findById(id).get()
     }
 }
