fe3898303e1c419bcb5a2c77304f9828cf88830f
cousim46 <wjdghldns24@naver.com>
2023-04-06 19:25:09 +0900

    team이 삭제가 되었을때에 대한 테스트 코드

--- api/src/test/kotlin/team/guin/team/TeamApiServiceTest.kt
+++ api/src/test/kotlin/team/guin/team/TeamApiServiceTest.kt
@@ -3,6 +3,7 @@ package team.guin.team
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.repository.findByIdOrNull
 import team.guin.account.AccountApiRepository
 import team.guin.domain.team.HashTag
 import team.guin.domain.team.TeamPart
@@ -18,6 +19,7 @@ import team.guin.util.createAccount
 class TeamApiServiceTest(
     private val accountApiRepository: AccountApiRepository,
     private val teamApiService: TeamApiService,
+    private val teamApiRepository: TeamApiRepository,
 ) : FreeSpec({
     "create" - {
         "템플릿, 해시태그, 역할, 주제, 오픈채팅 주소를 작성하면 팀이 생성된다." - {
@@ -59,6 +61,46 @@ class TeamApiServiceTest(
             team.hashTags.size shouldBe teamCreate.hashTags.size
             team.templates.size shouldBe teamCreate.templates.size
             team.parts.size shouldBe teamCreate.parts.size
+        }
+        "모집글이 삭제되면 템플릿, 역할, 해시태그가 같이 삭제된다." - {
+            // given
+            val account = accountApiRepository.createAccount()
+            val templates: List<TeamTemplate> = listOf(
+                TeamTemplate(TemplateType.TEXT, "test", mutableListOf()),
+                TeamTemplate(
+                    TemplateType.RADIOBOX,
+                    "test2",
+                    mutableListOf(TemplateOption.create("Yes"), TemplateOption.create("No")),
+                ),
+            )
+            val subject = "9in"
+            val subjectType = SubjectType.PROJECT
+            val content = "9in 프로젝트 구해요"
+            val openChatUrl = "http://9in-proejct.chat"
+            val parts = listOf(TeamPart.create("백엔드", 2))
+            val hashTags =
+                listOf(
+                    HashTag(subjectType, TagType.JAVA),
+                    HashTag(subjectType, TagType.MYSQL),
+                )
+            val teamCreate = TeamCreate(
+                subject = subject,
+                content = content,
+                openChatUrl = openChatUrl,
+                templates = templates,
+                hashTags = hashTags,
+                parts = parts,
+            )
+
+            val team = teamApiService.createTeam(account.id, teamCreate)
+            val saveTeamId = team.id
+
+            // when
+            teamApiRepository.delete(team)
+
+            // then
+            val findTeam = teamApiRepository.findByIdOrNull(saveTeamId)
+            findTeam shouldBe null
         }
     }
 })
