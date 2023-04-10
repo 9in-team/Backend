6544a6fc71e548904b6207342c361cc63677a099
cousim46 <wjdghldns24@naver.com>
2023-04-10 19:11:14 +0900

    코멘트 추가 및 메서드명 변경

--- api/src/main/kotlin/team/guin/team/dto/TeamTemplateResponse.kt
+++ api/src/main/kotlin/team/guin/team/dto/TeamTemplateResponse.kt
@@ -1,5 +1,6 @@
 package team.guin.team.dto
 
+import team.guin.domain.team.TeamTemplate
 import team.guin.domain.team.enumeration.TemplateType
 
 data class TeamTemplateResponse(
@@ -8,15 +9,13 @@ data class TeamTemplateResponse(
     val options: String?,
 ) {
     companion object {
-        fun entityToResponse(
-            type: TemplateType,
-            question: String,
-            options: String?,
+        fun create(
+            template: TeamTemplate,
         ): TeamTemplateResponse {
             return TeamTemplateResponse(
-                type = type,
-                question = question,
-                options = options,
+                type = template.type,
+                question = template.question,
+                options = template.options,
             )
         }
     }
