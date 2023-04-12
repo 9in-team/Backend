bc4560fc58e08ea57c276a6c584ef04d83f3d2ca
deepredk <deepred.kim@gmail.com>
2023-04-12 21:43:37 +0900

    refacotr: 정상 동작 및 리팩토링중

--- api/src/main/kotlin/team/guin/security/SecurityConfig.kt
+++ api/src/main/kotlin/team/guin/security/SecurityConfig.kt
@@ -4,8 +4,8 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
+import team.guin.domain.account.enumeration.AccountRole
 
 @Configuration
 @EnableWebSecurity
@@ -13,18 +13,14 @@ class SecurityConfig {
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         return http
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
-            .sessionFixation().migrateSession()
-            .maximumSessions(1).maxSessionsPreventsLogin(false)
-            .and().and()
+            .sessionManagement().and()
             .cors().and()
             .csrf().disable()
             .authorizeHttpRequests()
             .antMatchers("/login").permitAll()
             .antMatchers("/**").permitAll()
-//            .antMatchers("/api/**").hasRole(AccountRole.USER.toString())
-//            .antMatchers("/api/admin").hasRole(AccountRole.ADMIN.toString())
+            .antMatchers("/api/**").hasRole(AccountRole.USER.toString())
+            .antMatchers("/api/admin").hasRole(AccountRole.ADMIN.toString())
             .anyRequest().authenticated()
             .and()
             .build()
