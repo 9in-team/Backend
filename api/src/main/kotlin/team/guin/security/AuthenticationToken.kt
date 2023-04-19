2054449dacd1b060651b6fb9ac5f0e4ecbf1dfa0
deepredk <deepred.kim@gmail.com>
2023-04-19 10:52:29 +0900

    feat: Spring Security를 통해 모든 API에 대해 로그인이 되어있는지 선 검증

---
+++ api/src/main/kotlin/team/guin/security/AuthenticationToken.kt
@@ -0,0 +1,46 @@
+package team.guin.security
+
+import com.fasterxml.jackson.annotation.JsonIgnore
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.SpringSecurityCoreVersion
+import team.guin.domain.account.enumeration.AccountRole
+
+class AuthenticationToken(
+    private val name: String,
+) : Authentication {
+    private var authorities = emptyList<GrantedAuthority>()
+    private var authenticated: Boolean = true
+
+    constructor() : this("")
+    constructor(username: String, role: AccountRole) : this(username) {
+        authorities = listOf(CustomGrantedAuthority(role.toString()))
+    }
+
+    @JsonIgnore override fun getCredentials(): Any? = null
+
+    @JsonIgnore override fun getDetails(): Any? = null
+
+    @JsonIgnore override fun getPrincipal(): Any = name
+
+    @JsonIgnore override fun isAuthenticated(): Boolean = true
+
+    override fun setAuthenticated(authenticated: Boolean) {
+        this.authenticated = authenticated
+    }
+
+    override fun getName(): String = name
+    override fun getAuthorities() = authorities.toList()
+}
+
+data class CustomGrantedAuthority(val role: String = "") : GrantedAuthority {
+    private val authority = role
+
+    companion object {
+        private const val serialVersionUID = SpringSecurityCoreVersion.SERIAL_VERSION_UID
+    }
+
+    override fun getAuthority(): String {
+        return role
+    }
+}
