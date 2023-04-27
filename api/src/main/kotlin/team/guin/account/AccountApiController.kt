430ae400e82ba2c8819f4bd5f4dc5d64d22ed12b
deepredk <deepred.kim@gmail.com>
2023-04-27 21:30:28 +0900

    refactor: 컨트롤러단에 있던 비즈니스 로직 서비스단으로 이동

--- api/src/main/kotlin/team/guin/account/AccountApiController.kt
+++ api/src/main/kotlin/team/guin/account/AccountApiController.kt
@@ -12,15 +12,12 @@ import team.guin.account.dto.AccountJoinRequest
 import team.guin.account.dto.AccountUpdateRequest
 import team.guin.common.CommonResponse
 import team.guin.security.kakao.AccountProfile
-import team.guin.security.kakao.KakaoApiService
 import javax.servlet.http.HttpSession
 
 @RestController
 @RequestMapping("/account")
 class AccountApiController(
     private val accountApiService: AccountApiService,
-    private val kakaoApiService: KakaoApiService,
-    private val accountApiRepository: AccountApiRepository,
 ) {
     @PostMapping
     fun join(
@@ -28,11 +25,10 @@ class AccountApiController(
         httpSession: HttpSession,
     ): CommonResponse<AccountProfile> {
         val accessToken = accountJoinRequest.accessToken
-        val kakaoDetailProfile = kakaoApiService.fetchKakaoDetailProfile(accessToken)
-        val account = accountApiRepository.findByEmail(kakaoDetailProfile.email)
-            ?: accountApiRepository.save(kakaoDetailProfile.toEntity())
-        val accountProfile = AccountProfile.from(account)
+
+        val accountProfile = accountApiService.joinOrLogin(accessToken)
         httpSession.setAttribute("USER", accountProfile)
+
         return CommonResponse.okWithDetail(accountProfile)
     }
 
