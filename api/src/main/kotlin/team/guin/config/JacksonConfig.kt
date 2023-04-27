bb01463e8bf019af7bab59097e863e0ba7781320
deepredk <deepred.kim@gmail.com>
2023-04-27 21:23:22 +0900

    fix: ObjectMapper 역직렬화 시 널이 될 수 없는 타입의 변수의 입력이 비었을때 예외가 발생하지 않는 문제 수정

--- api/src/main/kotlin/team/guin/config/JacksonConfig.kt
+++ api/src/main/kotlin/team/guin/config/JacksonConfig.kt
@@ -1,6 +1,7 @@
 package team.guin.config
 
 import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -11,6 +12,7 @@ class JacksonConfig {
     fun objectMapper(): ObjectMapper {
         val objectMapper = ObjectMapper()
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL)
+        objectMapper.enable(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES)
         return objectMapper
     }
 }
