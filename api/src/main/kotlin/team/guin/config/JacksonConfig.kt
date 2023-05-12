dd7715fab1483f1dad650fdfc63dac152b02b08f
deepredk <deepred.kim@gmail.com>
2023-05-12 16:43:21 +0900

    fix: 기본생성자가 없어도 역직렬화 가능하게끔 수정

--- api/src/main/kotlin/team/guin/config/JacksonConfig.kt
+++ api/src/main/kotlin/team/guin/config/JacksonConfig.kt
@@ -3,6 +3,7 @@ package team.guin.config
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -13,6 +14,7 @@ class JacksonConfig {
         val objectMapper = ObjectMapper()
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL)
         objectMapper.enable(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES)
+        objectMapper.registerKotlinModule()
         return objectMapper
     }
 }
