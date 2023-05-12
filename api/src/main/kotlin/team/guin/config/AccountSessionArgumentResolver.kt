09eb6d003d0b8128f1d73a7da6f0fe23b8eb65c6
deepredk <deepred.kim@gmail.com>
2023-05-12 16:56:27 +0900

    fix: @AccountSession이 사용된 API Endpoint에서만 인증하도록 변경

--- api/src/main/kotlin/team/guin/config/AccountSessionArgumentResolver.kt
+++ api/src/main/kotlin/team/guin/config/AccountSessionArgumentResolver.kt
@@ -1,6 +1,7 @@
 package team.guin.config
 
 import org.springframework.core.MethodParameter
+import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.support.WebDataBinderFactory
 import org.springframework.web.context.request.NativeWebRequest
@@ -24,5 +25,6 @@ class AccountSessionArgumentResolver : HandlerMethodArgumentResolver {
         binderFactory: WebDataBinderFactory?,
     ): Any? {
         return webRequest.getAttribute("USER", RequestAttributes.SCOPE_SESSION) as? AccountProfile
+            ?: throw BadCredentialsException("로그인을 먼저 해주세요.")
     }
 }
--- api/src/main/kotlin/team/guin/config/annotation/AccountSession.kt
+++ api/src/main/kotlin/team/guin/config/annotation/AccountSession.kt
@@ -1,5 +1,8 @@
 package team.guin.config.annotation
 
+import org.springframework.security.access.prepost.PreAuthorize
+
 @Target(AnnotationTarget.VALUE_PARAMETER)
 @Retention(AnnotationRetention.RUNTIME)
+@PreAuthorize("USER")
 annotation class AccountSession
