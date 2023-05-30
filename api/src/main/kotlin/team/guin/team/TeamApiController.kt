42f67dbcb834077b3739904c524d6ae95c058b98
cousim46 <wjdghldns24@naver.com>
2023-05-30 19:35:47 +0900

    팀 목록 api

--- api/src/main/kotlin/team/guin/team/TeamApiController.kt
+++ api/src/main/kotlin/team/guin/team/TeamApiController.kt
@@ -5,12 +5,15 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import team.guin.common.CommonResponse
+import team.guin.domain.team.enumeration.SubjectType
 import team.guin.domain.team.enumeration.TagType
 import team.guin.team.dto.HashTagDetail
 import team.guin.team.dto.TeamCreateDetail
 import team.guin.team.dto.TeamCreateRequest
+import team.guin.team.dto.TeamDetail
 
 @RestController
 @RequestMapping("/team")
@@ -31,5 +34,11 @@ class TeamApiController(
                 study = TagType.studyTag(),
             ),
         )
+    }
+
+    @GetMapping
+    fun teamList(@RequestParam subjectType: SubjectType?): CommonResponse<List<TeamDetail>> {
+        val teams = teamApiService.findAllBySubjectType(subjectType)
+        return CommonResponse.okWithDetail(TeamDetail.toDetail(teams))
     }
 }
