eb4f6aa751f791746b14707c396ea8f6b6cd7e28
cousim46 <wjdghldns24@naver.com>
2023-05-30 19:35:08 +0900

    팀 목록 response

--- domain/src/main/kotlin/team/guin/domain/team/dto/AttributeDetail.kt
+++ domain/src/main/kotlin/team/guin/domain/team/dto/AttributeDetail.kt
@@ -1,6 +1,14 @@
 package team.guin.domain.team.dto
 
+import team.guin.domain.team.HashTag
+
 data class AttributeDetail(
     val name: String,
     val value: String,
-)
+) {
+    companion object {
+        fun create(hashtags: List<HashTag>): List<AttributeDetail> {
+            return hashtags.map { AttributeDetail(it.type.name, it.type.value) }
+        }
+    }
+}
