b6df95065d1e73dc4f2b11082963d1325abb16a0
deepredk <deepred.kim@gmail.com>
2023-06-28 23:16:22 +0900

    feat: 애플 로그인 시 세션 저장

--- api/src/main/kotlin/team/guin/login/apple/AppleLoginApiController.kt
+++ api/src/main/kotlin/team/guin/login/apple/AppleLoginApiController.kt
@@ -1,18 +1,24 @@
 package team.guin.login.apple
 
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import team.guin.common.CommonResponse
 import team.guin.login.apple.dto.AppleLoginRequest
 import team.guin.login.dto.AccountProfile
+import javax.servlet.http.HttpSession
 
 @RestController
 class AppleLoginApiController(
     private val appleLoginApiService: AppleLoginApiService
 ) {
     @PostMapping(value = ["/loginWithApple"])
-    fun joinOrLogin(appleLoginRequest: AppleLoginRequest): CommonResponse<AccountProfile> {
+    fun joinOrLogin(
+        @RequestBody appleLoginRequest: AppleLoginRequest,
+        httpSession: HttpSession,
+    ): CommonResponse<AccountProfile> {
         val accountProfile = appleLoginApiService.joinOrLogin(appleLoginRequest.id_token)
+        httpSession.setAttribute("USER", accountProfile)
 
         return CommonResponse.okWithDetail(accountProfile)
     }
