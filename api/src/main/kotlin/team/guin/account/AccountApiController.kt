7ae1d899501a5a9109fbe3b328d9623399d37a9a
jhw1015 <jhw1015@bank-mall.co.kr>
2023-07-23 03:35:03 +0900

    불필요한 코드 삭제ˆ

--- api/src/main/kotlin/team/guin/account/AccountApiController.kt
+++ api/src/main/kotlin/team/guin/account/AccountApiController.kt
@@ -1,37 +1,21 @@
 package team.guin.account
 
 import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import team.guin.account.dto.AccountDetail
-import team.guin.account.dto.AccountJoinRequest
 import team.guin.account.dto.AccountUpdateRequest
 import team.guin.common.CommonResponse
 import team.guin.config.annotation.AccountSession
 import team.guin.login.dto.AccountProfile
-import team.guin.security.kakao.AccountProfile
-import javax.servlet.http.HttpSession
 
 @RestController
 @RequestMapping("/account")
 class AccountApiController(
     private val accountApiService: AccountApiService,
 ) {
-    @PostMapping
-    fun join(
-        @RequestBody accountJoinRequest: AccountJoinRequest,
-        httpSession: HttpSession,
-    ): CommonResponse<AccountProfile> {
-        val accessToken = accountJoinRequest.accessToken
-
-        val accountProfile = accountApiService.joinOrLogin(accessToken)
-        httpSession.setAttribute("USER", accountProfile)
-
-        return CommonResponse.okWithDetail(accountProfile)
-    }
 
     @PutMapping
     fun update(@AccountSession accountProfile: AccountProfile, @RequestBody accountUpdateRequest: AccountUpdateRequest): CommonResponse<AccountDetail> {
