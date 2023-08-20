4c1d5cc749d693ecbd6a57bb23795297b2e79473
choikangheon <choikangheon@naver.com>
2023-08-20 19:22:07 +0900

    [fix] DatabaseCleaner.kt 추가

---
+++ api/src/test/kotlin/team/guin/common/DatabaseCleaner.kt
@@ -0,0 +1,41 @@
+/*
+package team.guin.common
+
+import org.springframework.beans.factory.InitializingBean
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
+import javax.persistence.Table
+
+@Component
+class DatabaseCleaner : InitializingBean {
+
+    @PersistenceContext
+    private lateinit var entityManager: EntityManager
+
+    private var tableNames: MutableSet<String> = mutableSetOf()
+
+    override fun afterPropertiesSet() {
+        tableNames = entityManager.metamodel.entities
+            .filter { it.javaType.isAnnotationPresent(Table::class.java) }
+            .map { it.javaType.getAnnotation(Table::class.java).name }
+            .toMutableSet()
+    }
+
+    @Transactional
+    fun cleanUp() {
+        println("Cleaning up database...")
+        entityManager.flush()
+
+        // Assuming H2 database
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE").executeUpdate()
+
+        tableNames.forEach { tableName ->
+            entityManager.createNativeQuery("TRUNCATE TABLE $tableName RESTART IDENTITY").executeUpdate()
+        }
+
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate()
+    }
+}
+*/
--- api/src/test/kotlin/team/guin/wish/WishApiServiceTest.kt
+++ api/src/test/kotlin/team/guin/wish/WishApiServiceTest.kt
@@ -1,14 +1,10 @@
 package team.guin.wish
 
-import com.ninjasquad.springmockk.SpykBean
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FreeSpec
-import io.kotest.extensions.spring.SpringTestExtension
-import io.kotest.extensions.spring.SpringTestLifecycleMode
 import io.kotest.matchers.equality.shouldBeEqualToComparingFields
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.instanceOf
-import io.mockk.every
 import io.mockk.mockk
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
@@ -20,20 +16,20 @@ import team.guin.domain.wish.Wish
 import team.guin.team.TeamApiRepository
 import team.guin.util.createAccount
 import team.guin.util.createTeam
-import java.util.*
 import javax.transaction.Transactional
 
 @ExtendWith(SpringExtension::class)
 @Transactional
 @SpringBootTest
 class WishApiServiceTest(
-    val wishApiRepository: WishApiRepository,
     val wishApiService: WishApiService,
-    @SpykBean var accountApiRepository: AccountApiRepository,
-    @SpykBean var teamApiRepository: TeamApiRepository,
+    val teamApiRepository: TeamApiRepository,
+    val accountApiRepository: AccountApiRepository,
+//    val databaseCleaner: DatabaseCleaner,
 ) : FreeSpec({
-    extensions(SpringTestExtension(SpringTestLifecycleMode.Root))
-
+    beforeEach() {
+//        databaseCleaner.cleanUp()
+    }
     "WishApiService" - {
         "Add" - {
             "찜하기를 생성한다." {
@@ -48,37 +44,35 @@ class WishApiServiceTest(
                 create.account shouldBeEqualToComparingFields leader
                 create.team shouldBeEqualToComparingFields createTeam
             }
-        }
-        "account가 존재하지 않으면 IllegalArgumentException 예외를 던진다." {
-            // given
-            val accountId: Long = 1
-            val teamId: Long = 1
-            val wishCreateRequest = WishCreateRequest(teamId)
+            "account가 존재하지 않으면 IllegalArgumentException 예외를 던진다." {
+                // given
+                val accountId: Long = 1
+                val teamId: Long = 1
+                val wishCreateRequest = WishCreateRequest(teamId)
 
-            every { accountApiRepository.findById(accountId) } returns Optional.empty()
-            // when
-            val exception = shouldThrow<IllegalArgumentException> {
-                wishApiService.create(accountId, wishCreateRequest)
+                // when
+                val exception = shouldThrow<IllegalArgumentException> {
+                    wishApiService.create(accountId, wishCreateRequest)
+                }
+                // then
+                exception.message shouldBe "Account not found for ID: $accountId"
             }
-            // then
-            exception.message shouldBe "Account not found for ID: $accountId"
-        }
-        "Team이 존재하지 않으면 IllegalArgumentException 예외를 던진다." {
-            // given
-            val accountId: Long = 1
-            val teamId: Long = 1
-            val wishCreateRequest = WishCreateRequest(teamId)
+            "Team이 존재하지 않으면 IllegalArgumentException 예외를 던진다." {
+                // given
 
-            val mockAccount = mockk<Account>()
+                val leader = accountApiRepository.createAccount()
+                val teamId = 2L
+                val wishCreateRequest = WishCreateRequest(teamId)
 
-            every { accountApiRepository.findById(accountId) } returns Optional.of(mockAccount)
-            every { teamApiRepository.findById(teamId) } returns Optional.empty()
-            // when
-            val exception = shouldThrow<IllegalArgumentException> {
-                wishApiService.create(accountId, wishCreateRequest)
+                val mockAccount = mockk<Account>()
+
+                // when
+                val exception = shouldThrow<IllegalArgumentException> {
+                    wishApiService.create(leader.id, wishCreateRequest)
+                }
+                // then
+                exception.message shouldBe "Team not found for ID: $teamId"
             }
-            // then
-            exception.message shouldBe "Team not found for ID: $teamId"
         }
     }
 })
