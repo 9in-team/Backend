14a8f6d626a928a5c42feb83dce341f9693bfcda
Shin seongsu <tlstjdtn321@naver.com>
2023-12-17 13:55:07 +0900

    fix: 충돌나는 API 동기화 작업(머지)

--- api/src/main/kotlin/team/guin/team/TeamApiController.kt
+++ api/src/main/kotlin/team/guin/team/TeamApiController.kt
@@ -1,10 +1,11 @@
 package team.guin.team
 
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import team.guin.common.CommonResponse
 import team.guin.config.annotation.AccountSession
@@ -15,6 +16,7 @@ import team.guin.team.dto.HashTagDetail
 import team.guin.team.dto.TeamCreateDetail
 import team.guin.team.dto.TeamCreateRequest
 import team.guin.team.dto.TeamDetail
+import team.guin.team.dto.TeamListDetail
 
 @RestController
 @RequestMapping("/team")
@@ -38,8 +40,14 @@ class TeamApiController(
     }
 
     @GetMapping
-    fun teamList(@RequestParam subjectType: SubjectType?): CommonResponse<List<TeamDetail>> {
+    fun teamList(@RequestParam subjectType: SubjectType?): CommonResponse<List<TeamListDetail>> {
         val teams = teamApiService.findAllBySubjectType(subjectType)
-        return CommonResponse.okWithDetail(TeamDetail.toDetailList(teams))
+        return CommonResponse.okWithDetail(TeamListDetail.toDetailList(teams))
+    }
+
+    @GetMapping("{teamId}")
+    fun detail(@PathVariable teamId: Long): CommonResponse<TeamDetail> {
+        val team = teamApiService.getTeam(teamId)
+        return CommonResponse.okWithDetail(TeamDetail.detail(team))
     }
 }
